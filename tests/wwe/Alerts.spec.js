64053766d6a64ddc73028db2742f9e29a06d1602
Madhu <your-email@example.com>
2026-08-03 19:11:14 +0530

    done changes in alerts

--- tests/wwe/Alerts.spec.js
+++ tests/wwe/Alerts.spec.js
@@ -1,8 +1,8 @@
-import{test,expect}from"@playwright/test";
-test("verify simple alert",async({page})=>{
+import { test, expect } from "@playwright/test";
+test("verify simple alert", async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/javascript_alerts')
     page.locator('//button[text()="Click for JS Alert"]').click()
-    page.on("dialog",async dialog =>{
+    page.on("dialog", async dialog => {
         expect(dialog.message()).toBe("I am a JS Alert")
         expect(dialog.type()).toBe('alert')
         await dialog.accept()
@@ -11,20 +11,20 @@ test("verify simple alert",async({page})=>{
 
 })
 
-test("verify confirm alert",async({page})=>{
+test("verify confirm alert", async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/javascript_alerts')
     page.locator("//button[text()='Click for JS Confirm']").click()
-    page.on("dialog",async dialog=>{
+    page.on("dialog", async dialog => {
         expect(dialog.message()).toBe("I am a JS Confirm")
         expect(dialog.type()).toBe("confirm")
         await dialog.dismiss()
     })
 })
 
-test("verify prompt alert",async({page})=>{
+test("verify prompt alert", async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/javascript_alerts')
     page.locator("//button[text()='Click for JS Prompt']").click()
-      page.on("dialog",async dialog=>{
+    page.on("dialog", async dialog => {
         expect(dialog.message()).toBe("I am a JS prompt")
         expect(dialog.type()).toBe("prompt")
         await dialog.accept("Madhu")
